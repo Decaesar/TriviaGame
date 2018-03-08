b5ac36ea8d06b8185fbc4c280101b734ed7b189c
Mohammed  Fouad <moha@Mohammeds-MBP.cfl.rr.com>
2018-03-07 20:50:10 -0500

    upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+
+
+// create a questions & answers array //
+
+// define the variables //
+
+// define the dispaly questions function //
+
+// define the next questions function //
+
+// start function //
+
+// reset function //
+
